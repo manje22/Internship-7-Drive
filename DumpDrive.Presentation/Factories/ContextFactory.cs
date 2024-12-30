83aa410a31569f5a70bb7efb2d8c71e42acac2ed
manje22 <uvodicmariela@gmail.com>
2024-12-30 22:28:03 +0100

    implemented DbContextFactory

---
+++ DumpDrive.Presentation/Factories/ContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+using DumpDrive.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DumpDrive.Presentation.Factories
+{
+    public static class DbContextFactory
+    {
+        public static DumpDriveDbContext GetDumpDriveContext()
+        {
+            var options = new DbContextOptionsBuilder()
+                .UseNpgsql(ConfigurationManager.ConnectionStrings["DumpDrive"].ConnectionString)
+                .Options;
+
+            return new DumpDriveDbContext(options);
+        }
+    }
+}
