baa38db8569b49b9cda925516c30236f5f8e671f
manje22 <uvodicmariela@gmail.com>
2024-12-30 22:38:26 +0100

    implemented base repo class

---
+++ DumpDrive.Domain/Enums/Result.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DumpDrive.Domain.Enums
+{
+    public enum Result
+    {
+        Success,
+        Error,
+        NoChanges,
+        NotFound,
+        AlreadyExists
+    }
+}
---
+++ DumpDrive.Domain/Repositories/BaseRepository.cs
@@ -0,0 +1,29 @@
+﻿using DumpDrive.Data.Entities;
+using DumpDrive.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DumpDrive.Domain.Repositories
+{
+    public abstract class BaseRepository
+    {
+        protected readonly DumpDriveDbContext DbContext;
+
+        protected BaseRepository(DumpDriveDbContext dbContext) 
+        {
+            DbContext = dbContext;
+        }
+        
+        protected Result SaveChanges()
+        {
+            var hasChanges = DbContext.SaveChanges() > 0;
+            if (hasChanges)
+                return Result.Success;
+
+            return Result.NoChanges;
+        }
+    }
+}
