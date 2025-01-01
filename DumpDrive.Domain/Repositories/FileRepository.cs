6f18a80c6c0a397141658392422995809338c6b9
manje22 <uvodicmariela@gmail.com>
2025-01-01 22:23:55 +0100

    created FileRepository in domain layer

--- DumpDrive.Domain/Repositories/FileRepository.cs
+++ DumpDrive.Domain/Repositories/FileRepository.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using DumpDrive.Data.Entities;
+using DumpDrive.Data.Entities.Models;
+using DumpDrive.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using File = DumpDrive.Data.Entities.Models.File;
 
 namespace DumpDrive.Domain.Repositories
 {
-    internal class FileRepository
+    public class FileRepository : BaseRepository
     {
+        public FileRepository(DumpDriveDbContext dbContext) : base(dbContext) { }
+
+        public Result Add(File file)
+        {
+            DbContext.Files.Add(file);
+
+            return SaveChanges();
+        }
+
+        public Result Delete(int id)
+        {
+            var fileToBeDeleted = DbContext.Files.Find(id);
+            if (fileToBeDeleted is null)
+            {
+                return Result.NotFound;
+            }
+
+            DbContext.Files.Remove(fileToBeDeleted);
+
+            return SaveChanges();
+        }
+
+        public Result Update(File file, int id)
+        {
+            var fileToBeUpdated = DbContext.Files.Find(id);
+
+            if (fileToBeUpdated is null)
+            {
+                return Result.NotFound;
+            }
+
+            fileToBeUpdated.Name = file.Name;
+
+            return SaveChanges();
+        }
+
     }
 }
