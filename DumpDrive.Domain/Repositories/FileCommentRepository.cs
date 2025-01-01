b4b7dff44c3a0beb3c1ad4eda65fcd5f6053003f
manje22 <uvodicmariela@gmail.com>
2025-01-01 21:27:42 +0100

    created FileCommentRepository in domain layer

--- DumpDrive.Domain/Repositories/FileCommentRepository.cs
+++ DumpDrive.Domain/Repositories/FileCommentRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DumpDrive.Data.Entities;
+using DumpDrive.Data.Entities.Models;
+using DumpDrive.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,32 @@ using System.Threading.Tasks;
 
 namespace DumpDrive.Domain.Repositories
 {
-    internal class FileCommentRepository
+    public class FileCommentRepository : BaseRepository
     {
+        public FileCommentRepository(DumpDriveDbContext dbContext) : base(dbContext) { }
+
+        public Result Add(FileComment fileComment)
+        {
+            DbContext.FilesComment.Add(fileComment);
+
+            return SaveChanges();
+        }
+
+        public Result Delete(int id)
+        {
+            var commentToBeDeleted = DbContext.FilesComment.Find(id);
+            if (commentToBeDeleted is null)
+            {
+                return Result.NotFound;
+            }
+
+            DbContext.FilesComment.Remove(commentToBeDeleted);
+
+            return SaveChanges();
+        }
+
+        //get all comments of doc
+        
+        //get comments filtered by person idk
     }
 }
