2b0cb67f8f76f85685c72359c1f760d6c51298c9
manje22 <uvodicmariela@gmail.com>
2025-01-02 11:15:19 +0100

    created FolderRepository in domain layer

--- DumpDrive.Domain/Repositories/FolderRepository.cs
+++ DumpDrive.Domain/Repositories/FolderRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DumpDrive.Data.Entities;
+using DumpDrive.Data.Entities.Models;
+using DumpDrive.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,42 @@ using System.Threading.Tasks;
 
 namespace DumpDrive.Domain.Repositories
 {
-    internal class FolderRepository
+    public class FolderRepository : BaseRepository
     {
+        public FolderRepository(DumpDriveDbContext dbContext) : base(dbContext) { }
+
+        public Result Add(Folder folder)
+        {
+            DbContext.Folders.Add(folder);
+
+            return SaveChanges();
+        }
+
+        public Result Delete(int id)
+        {
+            var folderToBeDeleted = DbContext.Folders.Find(id);
+            if (folderToBeDeleted is null)
+            {
+                return Result.NotFound;
+            }
+
+            DbContext.Folders.Remove(folderToBeDeleted);
+
+            return SaveChanges();
+        }
+
+        public Result Update(Folder folder, int id)
+        {
+            var folderToBeUpdated = DbContext.Folders.Find(id);
+
+            if (folderToBeUpdated is null)
+            {
+                return Result.NotFound;
+            }
+
+            folderToBeUpdated.Name = folder.Name;
+
+            return SaveChanges();
+        }
     }
 }
