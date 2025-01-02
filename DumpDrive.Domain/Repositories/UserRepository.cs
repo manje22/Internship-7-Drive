2363cf20b15b4286c7090b2834ce3076b2546e22
manje22 <uvodicmariela@gmail.com>
2025-01-02 11:21:51 +0100

    created UserRepository in domain layer

--- DumpDrive.Domain/Repositories/UserRepository.cs
+++ DumpDrive.Domain/Repositories/UserRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DumpDrive.Data.Entities;
+using DumpDrive.Data.Entities.Models;
+using DumpDrive.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,43 @@ using System.Threading.Tasks;
 
 namespace DumpDrive.Domain.Repositories
 {
-    internal class UserRepository
+    public class UserRepository : BaseRepository
     {
+        public UserRepository(DumpDriveDbContext dbContext) : base(dbContext) { }
+
+        public Result Add(User user)
+        {
+            DbContext.Users.Add(user);
+
+            return SaveChanges();
+        }
+
+        public Result Delete(int id)
+        {
+            var userToBeDeleted = DbContext.Users.Find(id);
+            if (userToBeDeleted is null)
+            {
+                return Result.NotFound;
+            }
+
+            DbContext.Users.Remove(userToBeDeleted);
+
+            return SaveChanges();
+        }
+
+        public Result Update(User user, int id)
+        {
+            var userToBeUpdated = DbContext.Users.Find(id);
+
+            if (userToBeUpdated is null)
+            {
+                return Result.NotFound;
+            }
+
+            userToBeUpdated.Email = user.Email;
+            userToBeUpdated.Password = user.Password;
+
+            return SaveChanges();
+        }
     }
 }
